807f2b4757d0c89869d62be8d47f5656bd68523c
Babu Macharia <66896673+bmacharia@users.noreply.github.com>
2023-10-17 13:02:16 -0700

    Implement RollBar (#3)

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -96,8 +96,9 @@ cors = CORS(
 # Rollbar ----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 with app.app_context():
-   """init rollbar module"""
+    """init rollbar module"""
     rollbar.init(
+        # access token
         rollbar_access_token,
         # environment name
         'production',
@@ -105,9 +106,6 @@ with app.app_context():
         root=os.path.dirname(os.path.realpath(__file__)),
         # flask already sets up logging
         allow_logging_basic_config=False)
-
-    # send exceptions from `app` to rollbar, using flask's signal system.
-    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
