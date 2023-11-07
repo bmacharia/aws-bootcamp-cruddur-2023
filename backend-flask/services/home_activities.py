e7e45fed00ed6d60f68da5ea5cd0a8b059afe170
bmacharia <babu.macharia@gmail.com>
2023-11-07 20:32:46 +0000

    Postgres conection pool/query

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
+from lib.db import pool
+
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
@@ -10,6 +12,33 @@ class HomeActivities:
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
+      sql = """
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """
+      print(sql)
+      span.set_attribute("app.result_length", len(results))
+
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone() 
+      return json[0]
+      return results
 
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
