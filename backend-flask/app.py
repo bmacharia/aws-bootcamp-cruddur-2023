7fbe58cdc86e8c0f46e6e88760b11d06751beeca
bmacharia <babu.macharia@gmail.com>
2023-11-09 00:07:40 +0000

    updated home and app.py files

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -178,6 +178,12 @@ def data_home():
     data = HomeActivities.run()
   return data, 200
 
+
+@app.route("/api/activities/notifications", methods=['GET'])
+def data_notifications():
+  data = NotificationsActivities.run()
+  return data, 200
+
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 @xray_recorder.capture('activities_users')
 def data_handle(handle):
@@ -230,3 +236,4 @@ def data_activities_reply(activity_uuid):
 
 if __name__ == "__main__":
   app.run(debug=True)
+  
