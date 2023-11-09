be841daabe95560c60db42792aad583f2e302ba2
bmacharia <babu.macharia@gmail.com>
2023-11-09 00:46:16 +0000

    install aws amplify

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,15 +3,15 @@ from opentelemetry import trace
 
 from lib.db import pool
 
-tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    with tracer.start_as_current_span("home-activites-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
 
       sql = """
       SELECT * FROM activities
@@ -22,6 +22,8 @@ class HomeActivities:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchone() 
+          json = cur.fetchall()
+      print("--------------")
+      print(json)
       return json[0]
       return results
