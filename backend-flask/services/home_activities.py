642bb4fa8aef35c54ba684b1096f57da5cdce9f8
bmacharia <babu.macharia@gmail.com>
2023-11-09 02:38:30 +0000

    updated home_activities.py

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
+from lib.db import pool, query_wrap_object, query_wrap_array
+
 
 #tracer = trace.get_tracer("home.activities")
 
