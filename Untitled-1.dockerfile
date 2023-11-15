4219ac109c132c9ba10170b61613ac0e00247f0c
bmacharia <babu.macharia@gmail.com>
2023-11-15 21:20:25 +0000

    update sg rule

--- Untitled-1.dockerfile
+++
@@ -1,8 +0,0 @@
---master-user-password 
-
-export CONNECTION_URL="postgresql://postgres:<>@localhost:5432/cruddur"
-gp env CONNECTION_URL="postgresql://postgres:<>@localhost:5432/cruddur"
-
-export PROD_CONNECTION_URL="postgresql://cruddurroot:<>@cruddur-db-instance.cusewyozdrdt.us-west-2.rds.amazonaws.com:5432/cruddur"
-
-gp env PROD_CONNECTION_URL="postgresql://cruddurroot:<>@cruddur-db-instance.cusewyozdrdt.us-west-2.rds.amazonaws.com:5432/cruddur"
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
-
+from lib.db import pool,query_wrap_array
 #tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
@@ -14,17 +13,30 @@ class HomeActivities:
     #  span.set_attribute("app.now", now.isoformat())
 
       #span.set_attribute("app.result_length", len(results))
-      sql = """
-      SELECT * FROM activities
-      """
+      sql = query_wrap_array ("""
+      SELECT 
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
       with pool.connection() as conn:
         with conn.cursor() as cur:
           cur.execute(sql)
           # this will return a tuple
           # the first field being the data
-          json = cur.fetchall()
-      print("==-------")
-      print("==-------")
-      print(json)
+          json = cur.fetchone()
+      print("---------")
+      print(json[0])
       return json[0]
       return results
