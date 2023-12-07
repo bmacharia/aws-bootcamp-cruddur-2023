c07b0756ff687d75dbc896882cea151e12056b78
bmacharia <babu.macharia@gmail.com>
2023-12-07 01:01:58 +0000

    created new cruddur user in DB

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,8 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
   ('Babu Macharia','babu.macharia@gmail.com', 'bmacharia', 'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+  ('Irene Macharia','babu.macharia@protonmail.com', 'imacharia', 'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co','bayko','MOCK');
   
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
