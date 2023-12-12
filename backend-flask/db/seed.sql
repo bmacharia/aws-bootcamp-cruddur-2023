44021f0a28d29e716ea54a760b4fe0453c009207
bmacharia <babu.macharia@gmail.com>
2023-12-12 19:53:02 +0000

    implement ddb still wip

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,11 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
   ('Babu Macharia','babu.macharia@gmail.com', 'bmacharia', 'MOCK'),
-  ('Irene Macharia','babu.macharia@protonmail.com', 'imacharia', 'MOCK'),
-  ('Dragon Fly','dragon@silence.com','dragonfly','MOCK');
+  ('Irene Macharia','babu.macharia@protonmail.com', 'imbuki', 'MOCK');
   
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
